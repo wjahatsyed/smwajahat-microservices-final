c19e514b985b16b3d14f8877ec44f2aceb7a9f88
syedwajahat1 <syed.wajahat@backcountry.com>
2023-09-08 20:34:30 +0500

    Place order only when all products selected in the order are available in the inventory

--- inventory-service/src/main/java/com/smwajahat/adil/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/smwajahat/adil/inventoryservice/service/InventoryService.java
@@ -24,9 +24,9 @@ public class InventoryService {
     public List<InventoryResponse> isInStock(List<String> skuCode) {
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
-                    InventoryResponse.builder().skuCode(inventory.getSkuCode())
-                            .isInStock(inventory.getQuantity()>0)
-                            .build()
+                        InventoryResponse.builder().skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
                 )
                 .toList();
     }
