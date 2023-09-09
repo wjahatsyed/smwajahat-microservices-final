03b0de476846465970d8fe34201a6fa40954b122
syedwajahat1 <syed.wajahat@backcountry.com>
2023-09-10 02:24:33 +0500

    Service discovery using Eureka Server

---
+++ discovery-server-new/src/main/java/com/smwajahat/adil/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,18 @@
+package com.smwajahat.adil.discoveryserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Syed Muhammad Wajahat
+ * Creation date: 08/09/2023
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
