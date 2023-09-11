9ee1529ba53728eaa624a58e7f05822a8f873a37
syedwajahat1 <syed.wajahat@backcountry.com>
2023-09-11 16:36:26 +0500

    Working on Spring cloud API gateway

---
+++ api-gateway/src/main/java/org/smwajahat/adil/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package org.smwajahat.adil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Syed Muhammad Wajahat
+ * Creation date: 11/Sep/2023
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
